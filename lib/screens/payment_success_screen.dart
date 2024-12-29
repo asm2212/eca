2888cd83a5a35e0ef702d000ee2d64972d6e7b86
asm2212 <asmareadmasu0@gmail.com>
2024-12-29 10:28:23 +0300

    payment sucess

--- lib/screens/payment_success_screen.dart
+++ lib/screens/payment_success_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wpay/utils/constants.dart';
 
 class PaymentSuccessScreen extends StatefulWidget {
   const PaymentSuccessScreen({super.key});
@@ -10,6 +11,47 @@ class PaymentSuccessScreen extends StatefulWidget {
 class _PaymentSuccessScreenState extends State<PaymentSuccessScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    color: Constants.buttonColor,
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.05, // Position text
+                  left: 0,
+                  right: 0,
+                  child: Text(
+                    "Payment Receipt",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.15, 
+                  left: MediaQuery.of(context).size.width * 0.1,
+                  right: MediaQuery.of(context).size.width * 0.1,
+                  child: Image.asset(
+                    'assets/images/pay_rec.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
